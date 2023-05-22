f272e8e9092167ba39d54b9611fabc4f9c39887b
LughsClues <cuberfreak@gmail.com>
2023-05-22 13:37:55 +0200

    Create Bishop class

---
+++ lib/bishop.rb
@@ -0,0 +1,33 @@
+class Bishop < Piece
+  def initialize(color, position)
+    super(color, position)
+    @symbol = color == :white ? '♗' : '♝'
+  end
+
+  def valid_moves(board)
+    moves = []
+
+    # Define the bishop's diagonal movement
+    diagonals = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
+
+    diagonals.each do |dx, dy|
+      1.upto(7) do |step|
+        x, y = position
+        move = [x + step * dx, y + step * dy]
+
+        if board.valid_move?(move)
+          if board.empty?(move)
+            moves << move
+          elsif board.enemy?(move, color)
+            moves << move
+            break
+          else
+            break
+          end
+        end
+      end
+    end
+
+    moves
+  end
+end
