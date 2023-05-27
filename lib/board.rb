afdf77bb9ae189f0a4578b04bd7587f5b3cbb5ba
LughsClues <cuberfreak@gmail.com>
2023-05-27 14:35:08 +0200

    Write #simulate_move

--- lib/board.rb
+++ lib/board.rb
@@ -28,6 +28,18 @@ class Board
     move_piece!(start_pos, end_pos)
   end
 
+  def simulate_move(start_pos, end_pos)
+    captured_piece = move_piece!(start_pos, end_pos)
+    in_check = in_check?(self[end_pos].color)
+    
+    # Undo the move
+    move_piece!(end_pos, start_pos)
+    self[end_pos] = captured_piece
+    self[end_pos].position = end_pos if captured_piece
+
+    in_check
+  end
+
   def valid_move?(pos)
     pos.all? { |coord| coord.between?(0, 7) }
   end
@@ -68,11 +80,21 @@ class Board
     grid.flatten.compact.select { |piece| piece.color == color }
   end
 
+  def find_king(color)
+    king = grid.flatten.compact.find { |piece| piece.is_a?(King) && piece.color == color }
+    king || (raise 'king not found')
+  end
+
   def move_piece!(start_pos, end_pos)
     piece = self[start_pos]
+    captured_piece = self[end_pos] # Added this line to store the captured piece, if any
+
     self[end_pos] = piece
     self[start_pos] = nil
     piece.position = end_pos
+
+    # Return the captured piece so that it can be replaced if necessary
+    captured_piece
   end
 
   def [](pos)
@@ -85,7 +107,7 @@ class Board
     @grid[x][y] = value
   end
 
-  def setup_pieces
+ def setup_pieces
     # Setup pawns
     8.times do |i|
       @grid[1][i] = Pawn.new(:black, [1, i], self)
@@ -98,7 +120,7 @@ class Board
       @grid[0][i] = piece.new(:black, [0, i], self)
       @grid[7][i] = piece.new(:white, [7, i], self)
     end
-  end
+  end 
   
 end
 
